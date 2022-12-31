1ee49e3e0b14639ac5f3461997dc16cbb465076c
TimeTraverler <1242857339@qq.com>
2022-12-31 18:12:48 +0800

    Update lab2

---
+++ Lab2 N-body/PythonApplication/PythonApplication.py
@@ -0,0 +1,73 @@
+import taichi as ti
+ti.init(ti.gpu)
+
+n = 300
+G = 6.67e-11
+galaxy_size = 0.45
+planet_radius = 2
+dt = 1e-5
+substepping = 10
+
+r = ti.field(dtype = ti.f32, shape = n)
+m = ti.field(dtype = ti.f32, shape = n)
+p = ti.Vector.field(2, ti.f32, n)
+v = ti.Vector.field(2, ti.f32, n)
+f = ti.Vector.field(2, ti.f32, n)
+
+@ti.kernel
+def init():
+    center = ti.Vector([0.5, 0.5])
+    for i in range(n):
+        theta = ti.random() * 2 * ti.math.pi
+        d = (ti.sqrt(ti.random()) * 0.7 + 0.3) * galaxy_size
+        offset = d * ti.Vector([ti.cos(theta), ti.sin(theta)])
+        r[i] = (ti.random() + 0.5) * planet_radius
+        m[i] = r[i] ** 3;
+        p[i] = center + offset
+        v[i] = [-offset.y, offset.x]
+        v[i] *= 120 * (ti.sqrt(ti.random()) + 0.5)
+
+@ti.kernel
+def compute():
+   for i in range(n):
+       f[i] = ti.Vector([0.0, 0.0])
+   for i in range(n):
+       for j in range(i + 1, n):
+           r = ti.math.length(p[i] - p[j])
+           force = G * m[i] * m[j] * (r ** 3) * (p[j] - p[i])
+           f[i] += force
+           f[j] -= force
+
+@ti.kernel
+def pull(xpos:ti.f32, ypos:ti.f32):
+    pos = [xpos, ypos]
+    for i in range(n):
+        f[i] += 3000000 * (pos - p[i])
+
+@ti.kernel
+def push(xpos:ti.f32, ypos:ti.f32):
+    pos = [xpos, ypos]
+    for i in range(n):
+        f[i] += 3000000 * (p[i] - pos)
+
+@ti.kernel
+def update():
+   for i in range(n):
+       v[i] += dt * f[i] / m[i]
+       p[i] += dt * v[i]
+
+gui = ti.GUI("n-body", (512, 512))
+init()
+while gui.running:
+    compute()
+    for e in gui.get_events(ti.GUI.PRESS):
+        if e.key == ti.GUI.LMB:
+            pull(e.pos[0], e.pos[1])
+        if e.key == ti.GUI.RMB:
+            push(e.pos[0], e.pos[1])
+        if e.key == ti.GUI.ESCAPE:
+            gui.running = False
+    update()
+    gui.clear(0x112F41)
+    gui.circles(p.to_numpy(), color=0xffffff, radius=r.to_numpy())
+    gui.show()
---
+++ Lab2 N-body/README.md
@@ -0,0 +1,17 @@
+# N-body
+
+**多体天体模型，初始随机体积位置并环绕**
+
+**~~看起来...还行的样子~~**
+
+
+
+*鼠标左键 对所有天体施加引力*
+
+*鼠标右键 对所有天体施加斥力*
+
+*Esc 退出程序*
+
+
+
+![image](https://github.com/1242857339/Taichi-simulation/blob/main/Lab2%20N-body/show.png)
