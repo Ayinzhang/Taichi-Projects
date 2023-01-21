76d6eaf1554275f1f19f4c4c98d6014f850fdcff
TimeTraverler <1242857339@qq.com>
2023-01-21 17:46:39 +0800

    Do some Lab4

--- Lab4 Dominoes/PythonApplication/PythonApplication.py
+++ Lab4 Dominoes/PythonApplication/PythonApplication.py
@@ -1,12 +1,13 @@
 import taichi as ti
-from math import pi,sin,cos,sqrt
+from taichi.math import *
 ti.init(ti.gpu)
 
+dt = 1e-3
 vertices = ti.Vector.field(3, dtype = float, shape = 8)
 indices = ti.field(ti.i32, shape = 36)
 
-vertices[0] = (-0.05,-0.1,-0.1);vertices[1] = (-0.05,-0.1,0.1);vertices[2] = (0.05,-0.1,0.1);vertices[3] = (0.05,-0.1,-0.1)
-vertices[4] = (-0.05,0.1,-0.1);vertices[5] = (-0.05,0.1,0.1);vertices[6] = (0.05,0.1,0.1);vertices[7] = (0.05,0.1,-0.1)
+vertices[0] = (-0.05,-0.1,0.1);vertices[1] = (-0.05,-0.1,-0.1);vertices[2] = (0.05,-0.1,-0.1);vertices[3] = (0.05,-0.1,0.1)
+vertices[4] = (-0.05,0.1,0.1);vertices[5] = (-0.05,0.1,-0.1);vertices[6] = (0.05,0.1,-0.1);vertices[7] = (0.05,0.1,0.1)
 
 indices[0] = 0;indices[1] = 1;indices[2] = 2;indices[3] = 0;indices[4] = 2;indices[5] = 3;indices[6] = 0;indices[7] = 1;indices[8] = 4;
 indices[9] = 1;indices[10] = 4;indices[11] = 5;indices[12] = 1;indices[13] = 2;indices[14] = 5;indices[15] = 2;indices[16] = 5;indices[17] = 6
@@ -27,6 +28,7 @@ p10 = ti.Vector.field(3, dtype = float, shape = 8)
 p11 = ti.Vector.field(3, dtype = float, shape = 8)
 p12 = ti.Vector.field(3, dtype = float, shape = 8)
 w = ti.field(ti.f32, shape = 13)
+theta = ti.field(ti.f32, shape = 13)
 
 for j in range(8):
     p0[j] = vertices[j] + ti.Vector((-0.2, 0, - 0.2 - 0.1 * sqrt(3)))
@@ -52,13 +54,31 @@ camera.position(0, 3, 3)
 camera.lookat(0, 0, 0)
 scene.set_camera(camera)
 
+@ti.func
+def rotate(x:vec3, o:vec3, y:vec3, theta:ti.f32) -> vec3:
+    left = (x - o).normalized()
+    up = -(x - o).cross(y - o).normalized()
+    return o + (x - o).norm() * sin(theta) * up + (x - o).norm() * cos(theta) * left
+
 @ti.kernel
 def update():
-    p0[0] = p0[3]
+
+    p0[0] = rotate(p0[0],p0[3],p0[2],w[0])
+    p0[1] = p0[0] + p0[2] - p0[3]
+    p0[4] = rotate(p0[4],p0[3],p0[2],w[0])
+    p0[5] = p0[4] + p0[2] - p0[3]
+    p0[7] = rotate(p0[7],p0[3],p0[2],w[0])
+    p0[6] = p0[7] + p0[2] - p0[3]
+
+    w[0] = (p0[3] - p0[7]).normalized().cross(ti.Vector((0, -1, 0))).norm()
 
 while window.running:
     scene.ambient_light((0.1, 0.1, 0.1))
     scene.point_light(pos = (0, 3, -3), color = (1, 1, 1))
+
+    w[0] = 0.01
+    update()
+
     scene.mesh(p0, indices = indices, color = (1, 0.89, 0.8), two_sided = True)
     scene.mesh(p1, indices = indices, color = (1, 0.89, 0.8), two_sided = True)
     scene.mesh(p2, indices = indices, color = (1, 0.89, 0.8), two_sided = True)
