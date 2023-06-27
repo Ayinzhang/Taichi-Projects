fc6797f5b9f54f973f39f06c84691426af8a65dc
TimeTraverler <1242857339@qq.com>
2023-06-27 23:37:46 +0800

    Update Lab6

--- Lab6 Temperature-diffuse/PythonApplication/PythonApplication.py
+++ Lab6 Temperature-diffuse/PythonApplication/PythonApplication.py
@@ -1,12 +1,12 @@
 import taichi as ti
 ti.init(ti.cpu)
 
-k = 50
+k = 3000
 n = 512
 dt = 1e-2
-radius = 5
+radius = 64
 t_min = 0
-t_max = 100
+t_max = 300
 paused = False
 n2 = n * n
 c = dt * k
@@ -15,25 +15,23 @@ t1 = ti.field(ti.f32, n2)
 pixels = ti.Vector.field(3, ti.f32, (n, n))
 D_Builder = ti.linalg.SparseMatrixBuilder(n2, n2, 5 * n2)
 
-ind = lambda i, j: i*n+j
-
 @ti.kernel
 def fillDiffusionMatrixBuilder(A: ti.types.sparse_matrix_builder()):
     for i,j in ti.ndrange(n, n):
         num = 0.0
         if i - 1 >= 0:
-            A[i * n + j, ind(i - 1,j)] += c
+            A[i * n + j, (i - 1) * n + j] -= c
             num += c
         if i + 1 < n:
-            A[i * n + j, ind(i + 1,j)] += c
+            A[i * n + j, (i + 1) * n + j] -= c
             num += c
         if j - 1 >= 0:
-            A[i * n + j, ind(i,j - 1)] += c
+            A[i * n + j, i * n + j - 1] -= c
             num += c
         if j + 1 < n:
-            A[i * n + j, ind(i,j + 1)] += c
+            A[i * n + j, i * n + j + 1] -= c
             num += c
-        A[i * n + j, i * n + j] -= num - 1
+        A[i * n + j, i * n + j] += num + 1
 
 @ti.kernel
 def init():
@@ -45,8 +43,8 @@ def init():
 
 @ti.kernel
 def update_sourse():
-    for i in range (n // 2 - radius, n // 2 + radius + 1):
-        for j in range (n // 2 - radius, n // 2 + radius + 1):
+    for i in range(n // 2 - radius, n // 2 + radius + 1):
+        for j in range(n // 2 - radius, n // 2 + radius + 1):
             if (i - n // 2) ** 2 + (j - n // 2) ** 2 <= radius ** 2:
                 t0[i * n + j] = t_max
 
@@ -68,10 +66,13 @@ def cook(t: ti.template(), color: ti.template(), t_min: ti.f32, t_max: ti.f32):
             color[i, j][1] = 1 + 4 * (t_min + 0.75 * d - t[i * n + j]) / d
             color[i, j][2] = 0
 
-gui = ti.GUI("Tempreture-diffuse", (n, n))
+gui = ti.GUI("Temperature-diffuse", (n, n))
 init()
 fillDiffusionMatrixBuilder(D_Builder)
 D = D_Builder.build()
+solver = ti.linalg.SparseSolver(solver_type="LLT")
+solver.analyze_pattern(D)
+solver.factorize(D)
 
 while gui.running:
     for e in gui.get_events(ti.GUI.PRESS):
@@ -82,7 +83,7 @@ while gui.running:
         elif e.key == 'r':
             init()
     if not paused:
-        t1.from_numpy(D @ t0.to_numpy())
+        t1.from_numpy(solver.solve(t0))
         t0.copy_from(t1)
         update_sourse()
         cook(t0, pixels, t_min, t_max)
